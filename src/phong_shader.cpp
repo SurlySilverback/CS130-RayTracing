0103b688cab93e4395b599f7ee5f1016c035a07a
SurlySilverback <SurlySilverback@github.com>
2017-12-10 16:09:46 -0800

    Test05 working.

--- src/phong_shader.cpp
+++ src/phong_shader.cpp
@@ -7,7 +7,7 @@
 
 vec3 Phong_Shader::
 Shade_Surface(const Ray& ray,const vec3& intersection_point,
-    const vec3& same_side_normal,int recursion_depth) const
+    const vec3& same_side_normal, int recursion_depth) const
 {
     vec3 color;
     // TODO: determine the color
@@ -15,17 +15,40 @@ Shade_Surface(const Ray& ray,const vec3& intersection_point,
     // Phong shader consists of ambient + diffuse + specular;
     // We find the components separately, sum them, and return this colour.
 
-    vec3 ambient = world.ambient_color + world.ambient_intensity + color_ambient;
+    //vec3 ambient = world.ambient_color + world.ambient_intensity + color_ambient;
 
     // L = k_d * I * max( 0, dot(n,l) )
     // where L = Lambertian pixel colour, k_d = diffuse coefficient,
     // I = intensity of light source, n = unit vector of surface normal,
-    // l = unit vector pointing towards light source
-    vec3 diffuse = max( 0, dot( same_side_normal.normalized() , intersection_point.normalized() ) );
+    // l = unit vector pointing towards light source.
 
-    vec3 specular =
+    vec3 light_color;       // Stores the light intensity of each light source based on its distance from the surface.
+    vec3 diffuse = {0,0,0}; // Stores the final diffuse value.
 
-    color = ambient + diffuse + specular;
+    vec3 n = same_side_normal.normalized();
+    vec3 l;
+
+    // For all of the lights in the scene
+    for ( unsigned i = 0; i < world.lights.size(); ++i )
+    {
+      light_color = ( world.lights.at(i)->Emitted_Light( ray ) ) / ( ( world.lights.at(i)->position - intersection_point ).magnitude_squared() );
+
+      std::cout << "light_colour is " << light_color << std::endl;
+
+      l = (world.lights.at(i)->position - intersection_point).normalized();
+
+      std::cout << "l is " << l << std::endl;
+
+      // Calculate the pixel's total diffuse as the sum of all of the lights'
+      // effect on the intersection point.
+      diffuse += std::max( 0.0, dot( n , l ) ) * light_color * color_diffuse;
+
+      std::cout << "diffuse(sum) is " << diffuse << std::endl << std::endl;
+    }
+
+    //vec3 specular =
+
+    color = diffuse;
 
     return color;
 }
